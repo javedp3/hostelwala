d27222d7086881891ef3635302a3cf63f93742ef
javed <javedp3@gmail.com>
2024-08-28 01:50:32 +0530

    second changes

---
+++ lang/hi/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'अवैध क्रेडेन्शियल',
+    'password' => 'अवैध पासवर्ड',
+    'throttle' => 'बरेच लॉगिन प्रयत्न. कृपया सेकंद सेकंदात पुन्हा प्रयत्न करा.',
+
+];
